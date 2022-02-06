b0c1169bbd8ab48694b136ef4ee794da37f5969d
Izura <uchihaizura1219@gmail.com>
2022-02-07 02:17:16 +0300

    version 1.4
    new era

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-  const canvas = document.querySelector('canvas');
-  const renderer = new THREE.WebGLRenderer({
-    canvas,
-    alpha: true,
-  });
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
+//импорт библиотеки three.js
+import * as THREE from "https://cdn.skypack.dev/three@0.136.0";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.136.0/examples/jsm/loaders/GLTFLoader";
+import { FontLoader } from "https://cdn.skypack.dev/three@0.136.0/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "https://cdn.skypack.dev/three@0.136.0/examples/jsm/geometries/TextGeometry";
+//создание канваса, начальные настройки сцены, камеры и т.д.
+  const scene = new THREE.Scene();
 
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 5);
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1000);
   camera.position.z = 20;
 
-  const scene = new THREE.Scene();
-  //scene.background = new THREE.Color('slateblue');
+  const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
   const light = new THREE.DirectionalLight('blueviolet', 10);
   light.position.set(-2, 0, 20);
   scene.add(light);
 
-  //music
-  const body = document.querySelector('body'); 
+  //добавление музыки для атмосферности
+  const body = document.querySelector('body');
   const audioMac = new Audio('macintosh-plus-420.mp3');
+
+  audioMac.volume = 0.1;
 
   body.onclick = () =>{
     audioMac.play();
     body.style.cursor = 'default';
   }
-
-  const loader = new THREE.GLTFLoader();
+//загрузка модели и установление её настроек
+  const loader = new GLTFLoader();
 
   loader.load( 'scene.gltf', function ( gltf ) {
   gltf.scene.position.set(0,-2,15);
@@ -44,13 +47,13 @@
     requestAnimationFrame(render);
   }
   requestAnimationFrame(render);
-}); 
+});
+//загрузка текста и установление его настроек
+const loaderText = new FontLoader();
 
-const loaderr = new THREE.FontLoader();
+loaderText.load( 'helvetiker_regular.typeface.json', function ( font ) {
 
-loaderr.load( 'helvetiker_regular.typeface.json', function ( font ) {
-
-    const geometry = new THREE.TextGeometry( 'its all in your h e a d', {
+    const geometry = new TextGeometry( 'its all in your h e a d', {
     font: font,
     size: .2,
     height: .01,
@@ -64,18 +67,18 @@ loaderr.load( 'helvetiker_regular.typeface.json', function ( font ) {
     const textHate = new THREE.Mesh( geometry, material );
 
     textHate.position.x = 1.5;
-    textHate.position.z = 18;    
+    textHate.position.z = 18;
     textHate.position.y = -.1;
     scene.add(textHate);
 
      function render(time) {
     textHate.position.x -= 0.001;
     textHate.rotation.y -= 0.01;
-    
+
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
   }
-  requestAnimationFrame(render); 
+  requestAnimationFrame(render);
 
 } );
